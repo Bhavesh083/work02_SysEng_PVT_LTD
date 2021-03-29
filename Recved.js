78dd03702f463c59ba82dcc42a7455169cd60cb1
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-03-29 14:48:55 +0530

    Add files via upload

---
+++ Recved.js
@@ -0,0 +1,138 @@
+import React from 'react'
+import Redvedprops from './Redvedprops';
+import  '../styling/recved.css';
+
+function Recved() {
+    return (
+        <div className='rv-app'>
+            <h2>Recommended</h2>
+            <div className='cards-list'>
+            <Redvedprops 
+             date=' • 10 months ago'
+             view='2.5M views'
+             sng='Arijit Singh Music'
+             info= 'Shayad (Reprise) - Arijit Singh | Love Aaj Kal | Pritam | High Quality Audio'
+             icon='https://yt3.ggpht.com/ytc/AAUvwnidHqUvbyNhLeEooCEgY1_MLs63iU_e4qZBSa6e=s68-c-k-c0x00ffffff-no-rj'
+             img='https://i.ytimg.com/vi/pcXJZ79xHQM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB2W5a689nrD8TRIJwxTd2D0fna8A'
+            />
+            <Redvedprops 
+             date= '• 2 month ago'
+             view= '45M'
+             sng=  'Warner Bros'
+             info= 'Godzilla vs. Kong – Official Trailer'
+             icon= 'https://yt3.ggpht.com/ytc/AAUvwnhIJZKqH3FkWGKNP-OufNVlj7qHMVzPtIItQ8DYOg=s68-c-k-c0x00ffffff-no-rj '
+             img=  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0UrQuZMLlsV--fM4oVwEwF13WKhDbrwWcgw&usqp=CAU'
+            />
+            <Redvedprops 
+             date=   ' • 1 year ago'
+             view=   ' 11M views'
+             sng='NBA '
+             info=    'Top 10 Dunks of the Decade'
+             icon=   'https://yt3.ggpht.com/ytc/AAUvwngzR9BDCULpmG1LgU4-WdgCh2DFnjVw7m_S6wafig=s68-c-k-c0x00ffffff-no-rj '
+             img=    'https://i.ytimg.com/an_webp/ue1NT3QhuVU/mqdefault_6s_480x270.webp?du=3000&sqp=CPHQ44AG&rs=AOn4CLBPshPyrFKtqs47dCviVnlRvmlkqw '
+            />
+            <Redvedprops 
+             date=   ' • 3 Years Ago'
+             view=   '13M Views'
+             sng=' MetaBallStudios'
+             info=    ' (Movie) Monster Size Comparision 3D Anim'
+             icon=   ' https://yt3.ggpht.com/ytc/AAUvwnixIjrwv2sdW_QiFMKpyy3EKaIRD5XXpN-KA1Wjvg=s68-c-k-c0x00ffffff-no-rj'
+             img=    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH81u3vatj2og2C1_aeJtGOXIf7PZ6vs04Uw&usqp=CAU '
+            />
+            <Redvedprops 
+             date=' • 10 months ago'
+             view='2.5M views'
+             sng='Arijit Singh Music'
+             info= 'Shayad (Reprise) - Arijit Singh | Love Aaj Kal | Pritam | High Quality Audio'
+             icon='https://yt3.ggpht.com/ytc/AAUvwnidHqUvbyNhLeEooCEgY1_MLs63iU_e4qZBSa6e=s68-c-k-c0x00ffffff-no-rj'
+             img='https://i.ytimg.com/vi/pcXJZ79xHQM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB2W5a689nrD8TRIJwxTd2D0fna8A'
+            />
+            
+            <Redvedprops 
+             date= '• 2 month ago'
+             view= '45M'
+             sng=  'Warner Bros'
+             info= 'Godzilla vs. Kong – Official Trailer'
+             icon= 'https://yt3.ggpht.com/ytc/AAUvwnhIJZKqH3FkWGKNP-OufNVlj7qHMVzPtIItQ8DYOg=s68-c-k-c0x00ffffff-no-rj '
+             img=  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0UrQuZMLlsV--fM4oVwEwF13WKhDbrwWcgw&usqp=CAU'
+            />
+            <Redvedprops 
+             date=   ' • 1 year ago'
+             view=   ' 11M views'
+             sng='NBA '
+             info=    'Top 10 Dunks of the Decade'
+             icon=   'https://yt3.ggpht.com/ytc/AAUvwngzR9BDCULpmG1LgU4-WdgCh2DFnjVw7m_S6wafig=s68-c-k-c0x00ffffff-no-rj '
+             img=    'https://i.ytimg.com/an_webp/ue1NT3QhuVU/mqdefault_6s_480x270.webp?du=3000&sqp=CPHQ44AG&rs=AOn4CLBPshPyrFKtqs47dCviVnlRvmlkqw '
+            />
+            <Redvedprops 
+             date= '• 2 month ago'
+             view= '45M'
+             sng=  'Warner Bros'
+             info= 'Godzilla vs. Kong – Official Trailer'
+             icon= 'https://yt3.ggpht.com/ytc/AAUvwnhIJZKqH3FkWGKNP-OufNVlj7qHMVzPtIItQ8DYOg=s68-c-k-c0x00ffffff-no-rj '
+             img=  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0UrQuZMLlsV--fM4oVwEwF13WKhDbrwWcgw&usqp=CAU'
+            />
+            <Redvedprops 
+             date=' • 10 months ago'
+             view='2.5M views'
+             sng='Arijit Singh Music'
+             info= 'Shayad (Reprise) - Arijit Singh | Love Aaj Kal | Pritam | High Quality Audio'
+             icon='https://yt3.ggpht.com/ytc/AAUvwnidHqUvbyNhLeEooCEgY1_MLs63iU_e4qZBSa6e=s68-c-k-c0x00ffffff-no-rj'
+             img='https://i.ytimg.com/vi/pcXJZ79xHQM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB2W5a689nrD8TRIJwxTd2D0fna8A'
+            />
+            
+            <Redvedprops 
+             date=   ' • 3 Years Ago'
+             view=   '13M Views'
+             sng=' MetaBallStudios'
+             info=    ' (Movie) Monster Size Comparision 3D Anim'
+             icon=   ' https://yt3.ggpht.com/ytc/AAUvwnixIjrwv2sdW_QiFMKpyy3EKaIRD5XXpN-KA1Wjvg=s68-c-k-c0x00ffffff-no-rj'
+             img=    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH81u3vatj2og2C1_aeJtGOXIf7PZ6vs04Uw&usqp=CAU '
+            />
+            <Redvedprops 
+             date=' • 10 months ago'
+             view='2.5M views'
+             sng='Arijit Singh Music'
+             info= 'Shayad (Reprise) - Arijit Singh | Love Aaj Kal | Pritam | High Quality Audio'
+             icon='https://yt3.ggpht.com/ytc/AAUvwnidHqUvbyNhLeEooCEgY1_MLs63iU_e4qZBSa6e=s68-c-k-c0x00ffffff-no-rj'
+             img='https://i.ytimg.com/vi/pcXJZ79xHQM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB2W5a689nrD8TRIJwxTd2D0fna8A'
+            />
+            <Redvedprops 
+             date=   ' • 1 year ago'
+             view=   ' 11M views'
+             sng='NBA '
+             info=    'Top 10 Dunks of the Decade'
+             icon=   'https://yt3.ggpht.com/ytc/AAUvwngzR9BDCULpmG1LgU4-WdgCh2DFnjVw7m_S6wafig=s68-c-k-c0x00ffffff-no-rj '
+             img=    'https://i.ytimg.com/an_webp/ue1NT3QhuVU/mqdefault_6s_480x270.webp?du=3000&sqp=CPHQ44AG&rs=AOn4CLBPshPyrFKtqs47dCviVnlRvmlkqw '
+            />
+            <Redvedprops 
+             date= '• 2 month ago'
+             view= '45M'
+             sng=  'Warner Bros'
+             info= 'Godzilla vs. Kong – Official Trailer'
+             icon= 'https://yt3.ggpht.com/ytc/AAUvwnhIJZKqH3FkWGKNP-OufNVlj7qHMVzPtIItQ8DYOg=s68-c-k-c0x00ffffff-no-rj '
+             img=  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0UrQuZMLlsV--fM4oVwEwF13WKhDbrwWcgw&usqp=CAU'
+            />
+            
+            <Redvedprops 
+             date=   ' • 3 Years Ago'
+             view=   '13M Views'
+             sng=' MetaBallStudios'
+             info=    ' (Movie) Monster Size Comparision 3D Anim'
+             icon=   ' https://yt3.ggpht.com/ytc/AAUvwnixIjrwv2sdW_QiFMKpyy3EKaIRD5XXpN-KA1Wjvg=s68-c-k-c0x00ffffff-no-rj'
+             img=    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH81u3vatj2og2C1_aeJtGOXIf7PZ6vs04Uw&usqp=CAU '
+            />
+            <Redvedprops 
+             date=' • 10 months ago'
+             view='2.5M views'
+             sng='Arijit Singh Music'
+             info= 'Shayad (Reprise) - Arijit Singh | Love Aaj Kal | Pritam | High Quality Audio'
+             icon='https://yt3.ggpht.com/ytc/AAUvwnidHqUvbyNhLeEooCEgY1_MLs63iU_e4qZBSa6e=s68-c-k-c0x00ffffff-no-rj'
+             img='https://i.ytimg.com/vi/pcXJZ79xHQM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB2W5a689nrD8TRIJwxTd2D0fna8A'
+            />
+            </div>
+        </div>
+    )
+}
+
+export default Recved
