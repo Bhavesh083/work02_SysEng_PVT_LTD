5d7900e75b19162e5bdcca7a7c9666f21dc438e8
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-03-29 14:49:18 +0530

    Add files via upload

---
+++ Redvedprops.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import  '../styling/recved.css';
+
+function Redvedprops(props) {
+    return (
+        <div className='recved-card'>
+            <img className='rec-ved-img' src={props.img} />
+            <div className='recved-data'>
+                <div className='dt-icon'>
+                    <img src={props.icon} />
+                </div>
+                <div className='dt-text'>
+                    <h3>
+                       {props.info}
+                    </h3>
+                    <h5>{props.sng}</h5>
+                   <div className='det-span'> <span>{props.view}</span><span>{props.date}</span></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Redvedprops
