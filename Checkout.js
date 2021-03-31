d3e2b360d5f89c98bd45e2e9d8dcc7edf977a0ac
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-03-31 12:01:30 +0530

    Update Checkout.js

--- Checkout.js
+++ Checkout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Checkoutitem from './Checkoutitem'
-import './Checkout.css'
 
 function Checkout() {
     const cart = useSelector(state => state.items)
