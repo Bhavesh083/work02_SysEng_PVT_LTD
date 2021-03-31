2e9f33d5c9d2d32cd3a12263db05265814c7cc5c
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-03-31 12:02:08 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Header.css'
 import {Link} from 'react-router-dom'
 import SearchIcon from '@material-ui/icons/Search'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
