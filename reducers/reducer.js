81fe0cb05c6d876397344a960b337a3ad19e00fa
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-04-06 15:36:49 +0530

    Add files via upload

---
+++ reducers/reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+    items : [],
+    user : null,
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'Add': 
+            return {
+                ...state,
+                items : [...state.items,action.item]
+            }
+            break;
+        case 'Del':
+                const newItems = state.items
+                const ind = state.items.findIndex(item => item.id === action.item.id)
+                if(ind >= 0){
+                    newItems.splice(ind,1)
+                }else{
+
+                }
+            return {
+                ...state,
+                items : [...newItems]
+            }
+            break;
+        default:
+            return state;
+    }
+}
+export default reducer;
