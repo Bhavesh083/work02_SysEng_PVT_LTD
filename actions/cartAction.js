4ce35465b88fd4bcea95a5e96e11a5421d93848c
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-04-06 15:36:13 +0530

    Add files via upload

---
+++ actions/cartAction.js
@@ -0,0 +1,17 @@
+export const cartAdd = (id,title,rating,cost,img) => ({
+    type : 'Add',
+    item : {
+        id    : id,
+        title : title,
+        rating: rating,
+        cost  : cost,
+        img   : img,
+     }  
+})
+
+export const cartDel = (id) => ({
+    type : 'Del',
+    item : {
+        id    : id,
+    }
+})
